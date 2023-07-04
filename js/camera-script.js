a6d8ce18f407f5b5d38e74003e2c4e75f4f61549
OscarAglr <daniloaglr@gmail.com>
2023-07-04 16:31:23 -0600

    Stream de video y camara funcionales

--- js/camera-script.js
+++ js/camera-script.js
@@ -1,15 +1,27 @@
 let userContent = document.getElementById('user-content');
 let cameraButton = document.getElementById('cameraButton');
 let originalUC = userContent.innerHTML;
+let buttonPhoto = document.getElementById('button-photo');
+let originalBP = buttonPhoto.innerHTML;
+let takePhoto = document.getElementById('takePhotoButton');
+let canvas = document.getElementById('canvas');
+let video = document.getElementById('video-camera');
+let userIcon = document.getElementById('user-icon');
+let context = canvas.getContext('2d');
+var streaming = null;
 
 cameraButton.addEventListener("click", ()=>{
-    userContent.innerHTML = `<video id="video-camera" autoplay="true"></video>`;
-    let video = document.getElementById('video-camera');
+    cameraButton.classList.add('hidden');
+    takePhoto.classList.remove('hidden');
+    video.classList.remove('hidden');
+    userIcon.classList.add('hidden');    
 
     if (navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({video: true})
         .then(function(stream){
+            streaming = stream;
             video.srcObject = stream;
+            video.play();
         })
         .catch(function(error){
             console.log(error);
@@ -18,4 +30,20 @@ cameraButton.addEventListener("click", ()=>{
     else {
         alert('getUserMedia not supported :P');
     }
+    
+});
+
+function stopCamera() {
+    let track = streaming.getTracks()[0];
+    track.stop();
+    video.load();
+    streaming = null;
+}
+
+takePhoto.addEventListener("click", function() {
+    
+    canvas.classList.remove('hidden');
+    video.classList.add('hidden');
+    context.drawImage(video, 0, 0, 112, 112);
+    stopCamera();
 });
