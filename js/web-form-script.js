3cabcbecdfce9f9b98290d80bf8587fd42fc8f88
Fhernandov21 <69699672+Fhernandov21@users.noreply.github.com>
2023-07-05 21:54:00 -0600

    terminado el gestionar contenido (videos)

--- js/web-form-script.js
+++ js/web-form-script.js
@@ -9,9 +9,11 @@ let result_message = document.getElementById('result-message');
 let result_dir = document.getElementById('result-dir');
 let results = document.getElementsByClassName('results');
 
+let clear_btn = document.getElementById('clear-btn');
+
 function validate() {
     // Valida (casi) todos los campos del form
-    location.href = '#form-crearCuenta';
+    location.href = '#form-colab';
     if (!name.checkValidity()) {
         result_name.innerHTML = name.validationMessage;
     }
@@ -42,3 +44,11 @@ function clearResults() {
         results[i].innerHTML = "";
     }
 }
+
+clear_btn.addEventListener("click", ()=>{
+    location.href = '#form-colab';
+    name.value = "";
+    email.value = "";
+    message.value = "";
+    direction.value = "";
+});
